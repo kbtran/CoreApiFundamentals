7912093fc8a355fcb908c542beb6116df3319731
Bruce Tran <bruce.tran@gmail.com>
2020-03-06 23:54:18 -0500

    Update mapping to location object.

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -7,7 +7,15 @@ namespace CoreCodeCamp.Data
     { 
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>();
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(s => s.Location.VenueName))
+                .ForMember(c => c.Address1, o => o.MapFrom(s => s.Location.Address1))
+                .ForMember(c => c.Address2, o => o.MapFrom(s => s.Location.Address2))
+                .ForMember(c => c.Address3, o => o.MapFrom(s => s.Location.Address3))
+                .ForMember(c => c.CityTown, o => o.MapFrom(s => s.Location.CityTown))
+                .ForMember(c => c.StateProvince, o => o.MapFrom(s => s.Location.StateProvince))
+                .ForMember(c => c.PostalCode, o => o.MapFrom(s => s.Location.PostalCode))
+                .ForMember(c => c.Country, o => o.MapFrom(s => s.Location.Country));
         }
 
     }
