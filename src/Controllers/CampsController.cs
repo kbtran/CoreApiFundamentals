d15dc97b686359d407d02f6fede0c76f843d8445
Bruce Tran <bruce.tran@gmail.com>
2020-03-06 21:02:18 -0500

    Add CampsController

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+
+        public CampsController(ICampRepository campRepository)
+        {
+            _campRepository = campRepository;
+        }
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //   // if (false) return BadRequest("Bad stuff happens");
+        //    return Ok(new { Moniker = "ATL2020", Name = "Atlanta Code Camp" });
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                // get camps from database
+                var camps = await _campRepository.GetAllCampsAsync();
+
+                return Ok(camps);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+            
+        }
+    }
+}
