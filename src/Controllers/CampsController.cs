01113b818d4c60825e7e2d49a23222a8cfce2477
Bruce Tran <bruce.tran@gmail.com>
2020-03-08 00:56:25 -0500

    Add Post method , Mapper for CampModel to Camp, and LinkGenerator.

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -5,19 +5,23 @@ using CoreCodeCamp.Data;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore.Internal;
 
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _campRepository;
         private readonly IMapper _mapper;
-        public CampsController(ICampRepository campRepository, IMapper mapper)
+        private readonly LinkGenerator _linkGenerator;
+        public CampsController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _campRepository = campRepository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         //[HttpGet]
@@ -33,15 +37,15 @@ namespace CoreCodeCamp.Controllers
             try
             {
                 // get camps from database
-                var camps = await _campRepository.GetAllCampsAsync(includeTalks);
-           
+                Camp[] camps = await _campRepository.GetAllCampsAsync(includeTalks);
+
                 return _mapper.Map<CampModel[]>(camps);
             }
             catch (Exception)
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
-            
+
         }
 
         [HttpGet("{moniker}")]
@@ -49,7 +53,7 @@ namespace CoreCodeCamp.Controllers
         {
             try
             {
-                var camp = await _campRepository.GetCampAsync(moniker, includeTalks);
+                Camp camp = await _campRepository.GetCampAsync(moniker, includeTalks);
 
                 if (camp == null) return NotFound();
 
@@ -67,7 +71,7 @@ namespace CoreCodeCamp.Controllers
         {
             try
             {
-                var camps = await _campRepository.GetAllCampsByEventDate(theDate, includeTalks);
+                Camp[] camps = await _campRepository.GetAllCampsByEventDate(theDate, includeTalks);
 
                 if (!camps.Any()) return NotFound();
 
@@ -79,5 +83,37 @@ namespace CoreCodeCamp.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<ActionResult<CampModel>> Post(CampModel campModel)
+        {
+            //adds a new camp and return the location and data
+            try
+            {
+                // Get path of HttpGet
+                var location = _linkGenerator.GetPathByAction("Get",
+                   "Camps", new { moniker = campModel.Moniker });
+
+                if (string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+
+                Camp camp = _mapper.Map<Camp>(campModel);
+                _campRepository.Add(camp);
+
+                if(await _campRepository.SaveChangesAsync())
+                {
+                    return Created("", _mapper.Map<CampModel>(camp));
+                }
+             
+                return Ok();
+            }
+            catch (Exception e)
+            {
+               return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure"); 
+            }
+
+        }
+   
     }
 }
---
+++ src/Data/CampModelProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampModelProfile : Profile
+    {
+        public CampModelProfile()
+        {
+            this.CreateMap<CampModel, Camp>()
+                .ForPath(c => c.Location.LocationId, o => o.MapFrom(s => s.LocationId))
+                .ForPath(c => c.Location.VenueName, o => o.MapFrom(s => s.Venue))
+                .ForPath(c => c.Location.Address1, o => o.MapFrom(s => s.Address1))
+                .ForPath(c => c.Location.Address2, o => o.MapFrom(s => s.Address2))
+                .ForPath(c => c.Location.Address3, o => o.MapFrom(s => s.Address3))
+                .ForPath(c => c.Location.CityTown, o => o.MapFrom(s => s.CityTown))
+                .ForPath(c => c.Location.StateProvince, o => o.MapFrom(s => s.StateProvince))
+                .ForPath(c => c.Location.PostalCode, o => o.MapFrom(s => s.PostalCode))
+                .ForPath(c => c.Location.Country, o => o.MapFrom(s => s.Country));
+
+            this.CreateMap<TalkModel, Talk>()
+                .ReverseMap();
+
+            this.CreateMap<SpeakerModel, Speaker>()
+                .ReverseMap();
+        }
+
+    }
+}
