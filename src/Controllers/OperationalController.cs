13e3a4d542974f7482fc682168ca890f3ce487f4
Bruce Tran <bruce.tran@gmail.com>
2020-03-09 23:26:13 -0400

    Add Operational Controller.

---
+++ src/Controllers/OperationalController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationalController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public OperationalController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                IConfigurationRoot root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
